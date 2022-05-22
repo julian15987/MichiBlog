2b8b04105d72f21a80e797d8338b856efca41568
julian alvarez <julian15987@gmail.com>
2022-05-22 13:06:37 -0300

    Blog con login

--- MBlog/urls.py
+++ MBlog/urls.py
@@ -4,6 +4,11 @@ from . import views
 urlpatterns = [
     # blog posts
     path('', views.michi_posts, name='michi_posts'),
+
+    # logins
+    path('login', views.login_request, name='login'),
+    path('logout', views.logout_request, name='logout'),
+
 ]
 
 
--- MBlog/views.py
+++ MBlog/views.py
@@ -1,7 +1,40 @@
 from django.shortcuts import render, redirect, HttpResponse
 
-# Create your views here.
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, authenticate, logout
+from django.contrib import messages
+
+from .models import MichiPost
 
 
+# Blog
 def michi_posts(request):
-    return render(request, 'index.html')
+    posts = MichiPost.objects.filter(erased=False)
+    posts = posts.order_by('-created_at')
+
+    return render(request, 'index.html', {'posts': posts})
+
+
+# Logins
+def login_request(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Successfully Logged In")
+        else:
+            messages.error(request, "Invalid Credentials")
+            return redirect("/login")
+        return redirect("/")
+
+    return render(request, 'login.html')
+
+
+def logout_request(request):
+    logout(request)
+    messages.success(request, "Successfully logged out")
+    return redirect('/')
